90937b60a46e73b814b522ed855ab5cdbd67e69b
joancliment <45012580+joancliment@users.noreply.github.com>
2018-12-10 00:08:35 +0100

    commit

--- src/test/java/io/joan/unitpower/AddCalcOneTwoEmpty.java
+++ src/test/java/io/joan/unitpower/AddCalcOneTwoEmpty.java
@@ -15,4 +15,14 @@ public class AddCalcOneTwoEmpty {
 
     }
 
+    @Test
+    public void Suma_un_numero_calculadora () {
+        Calc suma = new Calc();
+        String numberEnt = "5.0";
+        String resultado1 = suma.AddStrings(numberEnt);
+        String expected = "5.0";
+        Assert.assertEquals(resultado1, expected);
+
+    }
+
 }
