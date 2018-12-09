c811f0680236f78a136c11a71d0f07af91a46b4e
joancliment <45012580+joancliment@users.noreply.github.com>
2018-12-10 00:07:45 +0100

    commit

--- src/main/java/io/joan/unitpower/Calc.java
+++ src/main/java/io/joan/unitpower/Calc.java
@@ -10,14 +10,18 @@ public class Calc {
 
         List<String> NumN;
         NumN = new ArrayList<String>();
+
         int Ext = 1;
-        int x = 1;
+        int x = 0;
         int y = 0;
         int z = 0;
         int t = 0;
+        int long1 = 0;
         int zAct = 1;
         int zAnt = 2;
-        int NumberSal = 0;
+        double NumberSal = 0;
+
+        String[ ] lista1 = numberEnt.split("\\.");
 
 
         boolean empty; if (numberEnt.isEmpty()) empty = true;
@@ -26,38 +30,32 @@ public class Calc {
         if (empty) {
             return "";
         } else
-            {
-            while (x < numberEnt.length())
-            {
-                for (y=x; zAct < zAnt ; y++)
-                {
+        {
+            long1 = (numberEnt.length() - 1);
 
+            while (x <= long1 )
+            {
+                for (y=x; (y <= long1 && zAct < zAnt) ; y++)
+                {
                     z = y + 1;
                     if (!Character.isDigit(Integer.parseInt(numberEnt.substring(y, z))))
                     {
-                        zAct = zAct + 1;
+                        if (!((numberEnt.substring(y, z)).contains(".")))
+                        {
+                            zAct = zAct + 1;
+                        }
                     }
-                }
-                if (!Character.isDigit(Integer.parseInt(numberEnt.substring(x, y))))
-                {
-                   x = y + 1;
-                   if (zAct > 1)
-                   {
-                        zAct = zAct - 1;
-                   }
 
-                } else
-                    {
-                      NumN.add(numberEnt.substring(x, y));
-                      zAnt = zAct + 1;
-                      x = y + 2;
-                    }
+                }
+
+                NumN.add(numberEnt.substring(x, z));
+                zAnt = zAct + 1;
+                x = z + 1;
 
             }
 
-              
-                for (t=0; t < zAct ; t++) {
-                    NumberSal = Integer.valueOf(NumN.get(t)) + NumberSal;
+                for (int i = 0; i <= NumN.size() - 1; i++) {
+                    NumberSal = Double.valueOf(NumN.get(i)) + NumberSal;
                 }
         }
 
