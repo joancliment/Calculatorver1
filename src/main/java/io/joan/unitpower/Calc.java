ed6f95cc32696e1dd0f4a2e3245ddc60da0c2d0a
joancliment <45012580+joancliment@users.noreply.github.com>
2018-12-10 19:30:10 +0100

    commit

--- src/main/java/io/joan/unitpower/Calc.java
+++ src/main/java/io/joan/unitpower/Calc.java
@@ -37,8 +37,10 @@ public class Calc {
         int long1 = 0;
         int zAct = 1;
         int zAnt = 2;
-        double NumberSal = 0;
+        double NumberSal1 = 0;
+        int NumberSal2 = 0;
         boolean hay_numero = false;
+        boolean hay_decimales = false;
         String Salida1 = "";
 
         String[ ] lista1 = numberEnt.split("\\.");
@@ -52,6 +54,7 @@ public class Calc {
         } else
         {
             long1 = (numberEnt.length());
+
             x = 0;
 
             while (x < long1 )
@@ -64,7 +67,7 @@ public class Calc {
                 while (y < long1 && zAct < zAnt ) {
 
                     z = y + 1;
-                    
+
                     if (isNumeric((numberEnt.substring(y, z))) == true) {
                         hay_numero = true;
                         zok = z;
@@ -86,20 +89,37 @@ public class Calc {
                     NumN.add(numberEnt.substring(yok, zok));
                     zAnt = zAct + 1;
                     x = z;
-            
                 } else {
-            
                     zAnt = zAct + 1;
                     x = z;
                 }
             }
 
+            hay_decimales = false;
+            NumberSal1 = 0.0;
+            NumberSal2 = 0;
+
             for (int i = 0; (i <= NumN.size() - 1); i++) {
-                NumberSal = Double.valueOf(NumN.get(i)) + NumberSal;
+                if ((NumN.get(i).contains("."))) {
+                    hay_decimales = true;
+                    NumberSal1 = Double.valueOf(NumN.get(i)) + NumberSal1;
+                }
+                else {
+                    NumberSal2 = Integer.valueOf(NumN.get(i)) + NumberSal2;
+                }
+
             }
         }
 
-        Salida1 = String.valueOf(NumberSal);
+        if (hay_decimales) {
+
+            NumberSal1 = NumberSal1 + NumberSal2;
+            Salida1 = String.valueOf(NumberSal1);
+        }
+        else
+        {
+            Salida1 = String.valueOf(NumberSal2);
+        }
 
         return Salida1;
 
